b72d420d330fe5c520856a63207f522995563812
coding sigma <ganesh21renuse3@gmail.com>
2024-02-07 16:45:19 +0530

    added action commands to buttons

--- PdfTools.py
+++ PdfTools.py
@@ -1,5 +1,13 @@
 from tkinter import *
 import PyPDF2
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+
+
+def upload_file():
+    f_types = [('Pdf files', '*.pdf')]
+    filename = filedialog.askopenfilename(filetypes=f_types)
+
 
 if __name__ == "__main__":
     root = Tk()
@@ -12,7 +20,7 @@ if __name__ == "__main__":
     frame.place(anchor='center', relx=0.5, rely=0.5)
 
     heading = Label(frame, text="PDF Tools", font=('Helvatical bold', 20), bg="light blue")
-    btn1 = Button(frame, text="Merge PDF", font=('Halvetical bold', 15), height=1, width=12, bg="Light green")
+    btn1 = Button(frame, text="Merge PDF", font=('Halvetical bold', 15), height=1, width=12, bg="Light green", command=lambda: upload_file())
     btn2 = Button(frame, text="Add Pages", font=('Halvetical bold', 15), height=1, width=12, bg="Light green")
     btn3 = Button(frame, text="Delete Pages", font=('Halvetical bold', 15), height=1, width=12, bg="Light green")
     btn4 = Button(frame, text="Separate Pages", font=('Halvetical bold', 15), height=1, width=12, bg="Light green")
@@ -24,6 +32,3 @@ if __name__ == "__main__":
     btn4.place(anchor='center', relx=0.72, rely=0.80)
 
     root.mainloop()
-
-
-
