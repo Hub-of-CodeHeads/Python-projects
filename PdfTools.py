5df2f2185adf1a9359ed8e14bcfc79e79584c32d
i-akshaym <akshay.malwade777@gmail.com>
2024-02-07 15:48:24 +0530

    created application window

--- PdfTools.py
+++ PdfTools.py
@@ -6,3 +6,9 @@ if __name__ == "__main__":
     root.configure(background='light blue')
     root.title("test image app")
     root.geometry("400x400")
+
+    frame = Frame(root, width=350, height=300, background="navy blue")
+    frame.pack()
+    frame.place(anchor='center', relax=0.5, rely=0.5)
+
+    root.mainloop()
